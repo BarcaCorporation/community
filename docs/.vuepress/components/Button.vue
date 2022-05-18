afdeeb5c7864884a4b32578db3ae8001e678bc02
ahsouza <annibalhsouza@gmail.com>
2022-05-18 04:37:29 -0300

    feat: new component button

---
+++ docs/.vuepress/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="btn" :style="{background: bgColor}">
+    Button
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    bgColor: {
+      type: String,
+      default: 'blue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .btn {
+    padding: 1rem 1rem 1rem 1rem;
+    color: white;
+    font-size: 3rem;
+    border: none;
+    box-shadow: 10px 5px 5px grey;
+    border-radius: 1rem;
+  }
+  .btn:active {
+    box-shadow: 1px 1px 1px grey;
+  }
+</style>
