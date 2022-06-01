25c3e9685bf344154c5905b5aa36e9eed0cbc9fb
ahsouza <annibalhsouza@gmail.com>
2022-06-01 18:49:00 -0300

    feat: add new component example card

---
+++ docs/.vuepress/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+     <h5 :title="title">Card</h4>
+     <h6 :subtitle="subtitle">Card</h4>
+     <h4 :content="content">Card</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: false,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    shareButtons: {
+      type: Boolean,
+      required: false,
+    }
+  },
+  computed: {},
+  methods: {},
+} 
+</script>
+
+<style></style>
